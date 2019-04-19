f29c66daccc1f18d1b13e81f6e597125a6b52d73
milenaonmaui <milenakari@yahoo.com>
2019-04-19 16:55:49 -0700

    Add class signup feature

--- app/controllers/groupclass_controller.rb
+++ app/controllers/groupclass_controller.rb
@@ -39,6 +39,19 @@ class GroupclassController < ApplicationController
         erb :'/groupclasses/show'
     end
 
+    patch '/groupclasses/:id/signup' do
+        if logged_in?
+            @groupclass = Groupclass.find_by(:id=>params[:id])
+            if @groupclass.students.size < @groupclass.max_students
+              @groupclass.students<< current_user
+              @groupclass.save
+            end
+            redirect to "/groupclasses/#{@groupclass.id}/show"
+        else
+            redirect to "/students/login"
+        end 
+    end
+    
     patch '/groupclasses/:id' do  
         @groupclass = Groupclass.find_by(:id=>params[:id])
         @groupclass.name = params[:name]
@@ -47,5 +60,7 @@ class GroupclassController < ApplicationController
         @groupclass.save
         redirect to "/groupclasses/#{@groupclass.id}/show"
     end
+
+    
     
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,15 @@ class StudentsController < ApplicationController
         erb :'/students/login'
     end
 
+    get '/students/show' do
+        if logged_in? && !isInstructor?
+            @student=current_user
+            erb :'/students/show'
+        else
+            redirect '/students/login'
+        end 
+    end
+
     post '/students/login' do
         @student = Student.find_by(:email => params[:email])
         if @student && @student.authenticate(params[:password])
